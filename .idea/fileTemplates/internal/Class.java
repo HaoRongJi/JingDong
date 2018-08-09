530339998fbafb1f19e2dec6ac71ae08eb52bf0f
HaoRongJi <783439516@qq.com>
2018-08-09 09:31:29 +0800

    jingdong

---
+++ .idea/fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+*Author:${USER}
+*Time:${DATA}  ${TIME}
+*Description:${NAME}
+*/
+#parse("File Header.java")
+public class ${NAME} {
+}
